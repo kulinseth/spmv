8c97b0be6687d09aadb0a4094a29ddc6d29f67c3
Kulin Seth <kulin_seth@apple.com>
2020-03-07 14:10:11 -0800

    adding poski

---
+++ poski-v1.0.0/poski-var.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export OSKIDIR=/Users/kulinseth/src/coding/spmv/poski-v1.0.0/
+export OSKIBUILD=/Users/kulinseth/src/coding/spmv/poski-v1.0.0/build/
+export POSKIDIR=/usr/local
+export LD_LIBRARY_PATH=/usr/local:/Users/kulinseth/src/coding/spmv/poski-v1.0.0/:
+export POSKISRC=.
+export POSKICC=gcc
+export POSKICFLAGS="-I/Users/kulinseth/src/coding/spmv/poski-v1.0.0//include -g -O2 -std=gnu99 -D_THREAD_SAFE   -msse3 "
+
